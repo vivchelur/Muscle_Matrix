dd456e334bdc3839c6b6fc27df4e1d29298a0b59
Vivek Chelur <vivek@viveks-mbp-2.myfiosgateway.com>
2024-07-26 14:16:17 -0400

    added backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "musclematrixapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "musclematrixappb0c098d5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,14 @@
 import '../stylesheets/login.css'
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
+import logo from '../assets/MuscleMatrixLogoWhite.png'
 
 
 export function Login() {
 
     return(<div className='login-body'>
         <div className='text-center'>
-            <Image src="src/assets/MuscleMatrixLogoWhite.png" className='mm_logo'/>
+            <Image src={logo} className='mm_logo'/>
             <h1 className='title'>Muscle</h1>
             <h1 className='title2'>Matrix</h1>
         </div>
